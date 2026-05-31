668656d03074e89e330b04c01a8a707e0f5e06bf
unknown <kalathiyajevin77@gmail.com>
2026-05-31 15:00:36 +0530

    Api Integration

--- src/app/api/leads/routes.ts
+++ src/app/api/leads/routes.ts
@@ -47,7 +47,7 @@ export async function POST(req: Request) {
   );
 
   const response = await fetch(
-    `${process.env.API_BASE_URL}/leads`,
+    `http://madhavsolutions.tech/api/leads`,
     {
       method: "POST",
       headers: {
